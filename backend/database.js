6d29f94d5fb8e1afbed1b93d9797f3849fd8f130
Carlos Henrique de Lima <carlos334lima@gmail.com>
2021-08-25 22:14:49 -0300

    backend finished

---
+++ backend/database.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+
+import { URI_DATABASE } from 'react-native-dotenv'
+
+const URI = "mongodb+srv://monneyRunners:RIbb0wMDzo6xupmh@dev.4jkas.mongodb.net/monneyRunners?retryWrites=true&w=majority";
+
+let options = {};
+
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+mongoose.set('useUnifiedTopology', true);
+
+mongoose
+  .connect(URI, options)
+  .then(() => console.log('DB started!'))
+  .catch((err) => console.log(err));
